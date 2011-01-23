21d9eec6516d2566936f9edc3c1a5442e5c18a0d
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-22 19:18:05 -0700

    updating the media config to make the <sep> and <hash> optional

--- config/media.php
+++ config/media.php
@@ -1,12 +1,17 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'route' => 'media/ko/<file><sep><hash>.<ext>',
+	'route' => 'media/ko/<file>(<sep><hash>).<ext>',
 	'regex' => array(
-		// Pattern to match the file path (without extension)
-		'file' => '([a-zA-Z\/\.])+',
+		/**
+		 * Pattern to match the file path (without extension)
+		 * This pattern will match any file path until one of the following:
+		 * - an extension is found
+		 * - a forward slash is found, followed by a version number (#.#.#) where # is one or more digits
+		 */
+		'file' => '(.*?)((?=(\.([a-zA-Z0-9]+)$))|(?=\/(?=([0-9]+\.){3})))',
 		// Match the separator between file and hash
-		'sep'  => '([\-])',
+		'sep'  => '([\/])(?=([0-9]+\.){3})',
 		// Match the hash that is not part of the media file
 		'hash' => '([a-zA-Z0-9\.])+(?=[\.][a-zA-Z0-9]+$)',
 		// Match the file extension (without the dot)
