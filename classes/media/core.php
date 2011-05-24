92fe5dd7f770181edf4a0bc9356ca5f397b89272
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-24 11:59:07 -0700

    added a Media class with Media::url() being a shortcut to reverse route media files

---
+++ classes/media/core.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Media_Core {
+
+	public static function url($filepath)
+	{
+		return Route::url('media', array(
+			'filepath' => $filepath,
+			'uid'      => Kohana::config('media')->uid,
+		));
+	}
+}
