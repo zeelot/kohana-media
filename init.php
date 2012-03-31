6b2f876bd7d0ebafccc6422d218f291ba40e8ef4
Sinan Eldem <sineld@siirevim.com>
2012-03-31 11:03:37 +0300

    $config = Kohana::config('media'); to $config = Kohana::$config->load('media');

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-$config = Kohana::config('media');
+$config = Kohana::$config->load('media');
 
 Route::set('media', $config->route, $config->regex)
 	->defaults(array(
